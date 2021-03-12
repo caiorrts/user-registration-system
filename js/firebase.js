0343e3676eed068f8829cb5d02eceea88fe299c2
Caio Souza <caiosouza@Caios-Mac-mini.local>
2021-03-12 18:23:11 -0500

    New user and User list functions done.

---
+++ js/firebase.js
@@ -0,0 +1,11 @@
+
+var firebaseConfig = {
+    apiKey: "AIzaSyCFQbB3-s7OvbKlzz0xf8jH7DXTrjSso9E",
+    authDomain: "user-registration-system-crrts.firebaseapp.com",
+    projectId: "user-registration-system-crrts",
+    storageBucket: "user-registration-system-crrts.appspot.com",
+    messagingSenderId: "451867787625",
+    appId: "1:451867787625:web:f10a95cb7f3a87525abdf8"
+};
+
+firebase.initializeApp(firebaseConfig);
--- js/user-list.js
+++ js/user-list.js
@@ -1,5 +1,10 @@
-const db = firebase.firestore();
+const db         = firebase.firestore();
+const storage    = firebase.storage();
+const storageRef = storage.ref();
+
 const userList = document.getElementById("user-list");
+let userDel = "";
+let imgDel  = "";
 
 init();
 
@@ -11,13 +16,16 @@ function init() {
 
         querySnapshot.forEach((doc) => {
             const user = doc.data();
-            output = `
-                    <div class="l-user flex">
-                        <div class="u-picture">IMG</div>
+            output += `
+                    <div class="l-user flex flex-ai-c" id="${user.id}" >
+                        <div class="u-picture" style="background-image:url('${user.imgURL}');"></div>
                         <div class="u-name">${user.firstName} ${user.lastName}</div>
                         <div class="u-email">${user.email}</div>
                         <div class="u-phone">${user.phone}</div>
                         <div class="u-address">${user.address}</div>
+                        <div class="u-actions">
+                            <button id="${user.id}" onClick="reply_click(this.id)">X</button>
+                        </div>
                     </div>`;
       });
 
@@ -25,26 +33,35 @@ function init() {
     });
 }
 
-/*
-function getUsers() {
-    db.collection("Users").orderBy("firstName").get().then(
-        function (querySnapshot) {
-            let output = "";
-
-            querySnapshot.forEach((doc) => {
-                const user = doc.data();
-                output = `
-                    <div class="l-user flex">
-                        <div class="u-picture">IMG</div>
-                        <div class="u-name">${user.firstName} ${user.lastName}</div>
-                        <div class="u-email">${user.email}</div>
-                        <div class="u-phone">${user.phone}</div>
-                        <div class="u-address">${user.address}</div>
-                    </div>`;
-            });
+function reply_click(clicked_id) {
+    
+    db.collection("Users")
+    .onSnapshot(function (querySnapshot) {
+        querySnapshot.forEach((doc) => {
+            const user = doc.data();
             
-            userList.innerHTML = output;
-        }
-    )
+            if (clicked_id == user.id) {
+                // var str = 'one:two;three';    
+                // str.split(':').pop().split(';')[0]; // returns 'two'
+                imgDel = user.imgURL.split('%2F').pop().split('?')[0];
+                userDel = clicked_id;
+                userDelete(userDel, imgDel);
+                console.log("imgDel: ", imgDel)
+            }
+      });
+    });
 }
-*/
+
+function userDelete(id, img){
+    console.log("img: ", img)
+    db.collection("Users").doc(id).delete().then(function() {
+        console.log("User deleted: ", id);
+        
+        storageRef.child('images/'+ img).delete().then(function() {
+            console.log("Image deleted: ", img);
+        })
+        .catch((error) => console.log("Error deleting image: ", img, " - ", error));
+    })
+    .catch((error) => console.log("Error deleting user: ", id, " - ", error));
+    
+}
